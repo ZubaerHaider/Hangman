979822e6f1cbc6b47b05b2862f00c785eff3aac0
Zubaer Haider <zhefti@gmail.com>
2024-09-27 22:34:39 +0600

    New Content added.

--- script.js
+++ script.js
@@ -194,4 +194,30 @@ const canvasCreator = () => {
   };
   return { initialDrawing, head, body, leftArm, rightArm, leftLeg, rightLeg };
 };
+//draw the man
+const drawMan = (count) => {
+  let { head, body, leftArm, rightArm, leftLeg, rightLeg } = canvasCreator();
+  switch (count) {
+    case 1:
+      head();
+      break;
+    case 2:
+      body();
+      break;
+    case 3:
+      leftArm();
+      break;
+    case 4:
+      rightArm();
+      break;
+    case 5:
+      leftLeg();
+      break;
+    case 6:
+      rightLeg();
+      break;
+    default:
+      break;
+  }
+};
 
